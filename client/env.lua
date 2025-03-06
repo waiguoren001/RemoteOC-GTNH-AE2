9832403feacb6b6453a095f3aa6a171a9eda1b7d
waiguoren <96926582+waiguoren001@users.noreply.github.com>
2025-03-06 13:16:24 +0800

    Update env.lua

--- client/env.lua
+++ client/env.lua
@@ -3,16 +3,16 @@ local env = {
     pollingInterval = 8,
 
     -- 服务器地址，用于获取命令和上报数据
-    baseUrl = "http://192.168.0.1:1030",
+    baseUrl = "http://zhuanshuweihu.top:32777",
 
     -- 客户端id，用于在多台客户端集群中识别该客户端，为空时则获取所有命令，单节点默认即可
     clientId = "client_01",
 
     -- 服务端令牌，防止第三方上报垃圾数据或非法获取数据，需要与服务器端相同
-    serverToken = "3d4ed77e60b1aac9c7e4ca7f3a5cb47b",
+    serverToken = "9c7e4ca7f3a5cb47b3d4ed77e60b1aac",
 
     -- ME控制器或ME接口地址
-    aeAddress = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+    aeAddress = "66fc19b8-799c-4b6c-9dcc-83a3df976b14",
 
     -- 分块上报时每块的大小
     chunkSize = 256,
